8195c0e2b756292c4b6fe42ea2ed9ee0e83b0134
Vikram Sen <vikramsen7974@gmail.com>
2025-08-16 21:41:21 +0530

    my local changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from app.routes import main
 
 def create_app():
     app = Flask(__name__)
+    app.register_blueprint(main)   # <-- register blueprint
 
     @app.route("/")
     def home():
